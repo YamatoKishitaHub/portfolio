dadfe88e4f9e0751c06fff8aa82fd7ad1c75a17c
YamatoKishita <yamatokishita@YamatoKishitanoMacBook-Pro.local>
2024-07-04 22:21:42 +0900

    update qualification info

--- frontend/components/skill/Qualification.tsx
+++ frontend/components/skill/Qualification.tsx
@@ -20,7 +20,7 @@ type QualificationType = {
 };
 
 const Qualification = () => {
-  const qualifications: QualificationType[] = [
+  const itQualifications: QualificationType[] = [
     {
       label: "ITパスポート",
       date: "2022.03（2022.02 受験）",
@@ -37,19 +37,9 @@ const Qualification = () => {
       icon: <MdSecurity color="#02853F" />,
     },
     {
-      label: "日本商工会議所簿記検定試験3級",
-      date: "2023.01（2023.01 受験）",
-      icon: <FaCalculator color="#B7D4E9" />,
-    },
-    {
       label: "G検定（JDLA Deep Learning for GENERAL 2023#2）",
       date: "2023.06（2023.05 受験）",
       icon: <GiArtificialIntelligence color="#118371" />,
-    },
-    {
-      label: "TOEIC Listening & Reading 755点",
-      date: "2023.07（2023.06 受験）",
-      icon: <RiEnglishInput color="#000064" />,
     },
     {
       label: "データサイエンティスト検定",
@@ -61,29 +51,70 @@ const Qualification = () => {
       date: "2024.03",
       icon: <BsFillPassportFill color="#223B7C" />,
     },
+    {
+      label: "応用情報技術者試験",
+      date: "2024.07（2024.4 受験）",
+      icon: <MdComputer color="#025DB8" />,
+    },
+  ];
+
+  const otherQualifications: QualificationType[] = [
+    {
+      label: "日本商工会議所簿記検定試験3級",
+      date: "2023.01（2023.01 受験）",
+      icon: <FaCalculator color="#B7D4E9" />,
+    },
+    {
+      label: "TOEIC Listening & Reading 755点",
+      date: "2023.07（2023.06 受験）",
+      icon: <RiEnglishInput color="#000064" />,
+    },
   ];
 
   return (
     <div>
       <Heading label="資格" />
       <div>
-        <List
-          sx={{ width: "100%", maxWidth: 1280, bgcolor: "background.paper", mx: "auto" }}
-          component="nav"
-          aria-labelledby="nested-list-subheader"
-          className="flex flex-wrap w-80 sm:w-[640px] lg:w-[960px] 2xl:w-[1280px]"
-        >
-          {qualifications.map((qualification, index: number) => (
-            <div key={index} className="w-80">
-              <ListItemButton style={{ cursor: "default" }}>
-                <ListItemIcon>
-                  <Icon icon={qualification.icon} />
-                </ListItemIcon>
-                <ListItemText primary={qualification.label} secondary={qualification.date} />
-              </ListItemButton>
-            </div>
-          ))}
-        </List>
+        <div>
+          <Heading label="IT系" h2 />
+          <List
+            sx={{ width: "100%", maxWidth: 1280, bgcolor: "background.paper", mx: "auto" }}
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+            className="flex flex-wrap w-80 sm:w-[640px] lg:w-[960px] 2xl:w-[1280px] pt-0 -mt-2"
+          >
+            {itQualifications.map((itQualification, index: number) => (
+              <div key={index} className="w-80">
+                <ListItemButton style={{ cursor: "default" }}>
+                  <ListItemIcon>
+                    <Icon icon={itQualification.icon} />
+                  </ListItemIcon>
+                  <ListItemText primary={itQualification.label} secondary={itQualification.date} />
+                </ListItemButton>
+              </div>
+            ))}
+          </List>
+        </div>
+        <div>
+          <Heading label="その他" h2 />
+          <List
+            sx={{ width: "100%", maxWidth: 1280, bgcolor: "background.paper", mx: "auto" }}
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+            className="flex flex-wrap w-80 sm:w-[640px] lg:w-[960px] 2xl:w-[1280px] pt-0 -mt-2"
+          >
+            {otherQualifications.map((otherQualification, index: number) => (
+              <div key={index} className="w-80">
+                <ListItemButton style={{ cursor: "default" }}>
+                  <ListItemIcon>
+                    <Icon icon={otherQualification.icon} />
+                  </ListItemIcon>
+                  <ListItemText primary={otherQualification.label} secondary={otherQualification.date} />
+                </ListItemButton>
+              </div>
+            ))}
+          </List>
+        </div>
       </div>
     </div>
   );
